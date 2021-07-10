0d2166bb4980b046eaf8cd9f2897e2c6ea3f8af7
Grant Whiteman <grantwhiteman95@gmail.com>
2021-07-10 21:50:19 +0100

    added printStatement for feature test and refactored

--- spec/bank-challenge.spec.js
+++ spec/bank-challenge.spec.js
@@ -4,9 +4,9 @@ describe('Bank-challenge', function() {
     it('prints statement after adding deposits/withdrawals', function() {
         let account = new Account()
         let expectedOutput = ('date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00')
-        account.deposit(1000.00, '10-01-2012')
-        account.deposit(2000.00, '13-01-2012')
-        account.withdraw(500.00, '14-01-2012')
+        account.deposit(1000.00, '10/01/2012')
+        account.deposit(2000.00, '13/01/2012')
+        account.withdraw(500.00, '14/01/2012')
         expect(account.printStatement()).toEqual(expectedOutput)
     })
 })
--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,26 @@
 const getDate = require('../src/getDate.js')
 module.exports = class Account {
-    constructor() {
+    constructor(balance = 0) {
+        this._balance = balance
         this._transactions = []
+        this._statement = 'date || credit || debit || balance'
     }
 
     deposit(amount, date = getDate()) {
-        this._transactions.push(['deposit', amount, date])
+        this._balance += amount
+        this._transactions.unshift([date, amount.toFixed(2), null, this._balance.toFixed(2)])
     }
 
     withdraw(amount, date = getDate()) {
-        this._transactions.push(['withdrawal', amount, date])
+        this._balance -= amount
+        this._transactions.unshift([date, null, amount.toFixed(2), this._balance.toFixed(2)])
+    }
+
+    printStatement() {
+        console.log(this._transactions)
+        for (let i = 0; i < this._transactions.length; i++) {
+            this._statement += `\n${this._transactions[i].join(' || ').replace('  ', ' ')}`
+        }
+        return this._statement
     }
 }
